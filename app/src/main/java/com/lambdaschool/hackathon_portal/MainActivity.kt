3297839fd5c9951fd864a327f00b1365ef397bd1
tjclawson <tjamesclawson@gmail.com>
2019-12-16 16:23:04 -0500

    Add login fragment, nav graph, and nav drawer skeleton
    Co-authored-by: 05nelsonm <mjnelso2@protonmail.com>
    Co-authored-by: msmaitran <itsmaitran@gmail.com>

--- app/src/main/java/com/lambdaschool/hackathon_portal/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/MainActivity.kt
@@ -2,11 +2,18 @@ package com.lambdaschool.hackathon_portal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        nav_host_fragment.setupWithNavController(navController)
+
     }
 }
