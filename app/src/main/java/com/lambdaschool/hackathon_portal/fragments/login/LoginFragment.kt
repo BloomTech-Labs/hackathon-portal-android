5a0943750875701ba90be425419163775b0ca89f
tjclawson <tjamesclawson@gmail.com>
2019-12-16 18:11:09 -0500

    Add login functionality to login fragment, need to refactor to use dagger
    Co-authored-by: 05nelsonm <mjnelso2@protonmail.com>
    Co-authored-by: msmaitran <itsmaitran@gmail.com>

---
+++ app/src/main/java/com/lambdaschool/hackathon_portal/fragments/login/LoginFragment.kt
@@ -0,0 +1,85 @@
+package com.lambdaschool.hackathon_portal.fragments.login
+
+import android.app.Dialog
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.auth0.android.Auth0
+import com.auth0.android.Auth0Exception
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.provider.AuthCallback
+import com.auth0.android.provider.VoidCallback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
+import com.lambdaschool.hackathon_portal.MainActivity
+import com.lambdaschool.hackathon_portal.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+    lateinit var auth0: Auth0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_login.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+
+        activity?.apply {
+            auth0 = Auth0(this)
+            WebAuthProvider.login(auth0).withScheme("demo").withAudience(String.format("https://%s/userinfo", getString(R.string.com_auth0_domain))).start(this, object : AuthCallback {
+                    override fun onFailure(dialog: Dialog) {
+                        runOnUiThread { dialog.show() }
+                    }
+
+                    override fun onFailure(exception: AuthenticationException) {
+                        runOnUiThread {
+                           // Toast.makeText(this, "Error: " + exception.message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onSuccess(credentials: Credentials) {
+                        runOnUiThread {
+                            Log.i("BIGBRAIN", "Login Successful")
+                            Log.i("BIGBRAIN", credentials.accessToken)
+                        }
+                    }
+                })
+        }
+    }
+
+    private fun logout() {
+        activity?.apply {
+            WebAuthProvider.logout(auth0!!).withScheme("demo").start(this, object : VoidCallback {
+                override fun onSuccess(payload: Void) {}
+                override fun onFailure(error: Auth0Exception) { //Log out canceled, keep the user logged in
+                   // showNextActivity()
+                }
+            })
+        }
+    }
+
+    companion object {
+        const val EXTRA_CLEAR_CREDENTIALS = "com.auth0.CLEAR_CREDENTIALS"
+        const val EXTRA_ACCESS_TOKEN = "com.auth0.ACCESS_TOKEN"
+    }
+}
