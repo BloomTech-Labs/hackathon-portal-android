55759bdca11fab965b4ebc027d89b74bd4fdbbfa
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-06 13:48:33 -0500

    clean up dashboard fragment

--- app/src/main/java/com/lambdaschool/hackathon_portal/fragments/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/fragments/dashboard/DashboardFragment.kt
@@ -1,7 +1,6 @@
 package com.lambdaschool.hackathon_portal.fragments.dashboard
 
 
-import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,20 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.auth0.android.Auth0Exception
-import com.auth0.android.authentication.AuthenticationException
-import com.auth0.android.provider.AuthCallback
 import com.auth0.android.provider.VoidCallback
 import com.auth0.android.provider.WebAuthProvider
-import com.auth0.android.result.Credentials
 import com.lambdaschool.hackathon_portal.App
 
 import com.lambdaschool.hackathon_portal.R
 import kotlinx.android.synthetic.main.fragment_dashboard.*
 import javax.inject.Inject
 
-/**
- * A simple [Fragment] subclass.
- */
 class DashboardFragment : Fragment() {
 
     @Inject
@@ -37,7 +30,6 @@ class DashboardFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
