244ab5264284e78d7615452d7fa63ff91de7eb20
tjclawson <tjamesclawson@gmail.com>
2020-01-08 18:16:20 -0500

    Add AddHackathonViewModel and bind method to ViewModelsModule

--- app/src/main/java/com/lambdaschool/hackathon_portal/di/ViewModelModule.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/di/ViewModelModule.kt
@@ -1,9 +1,12 @@
 package com.lambdaschool.hackathon_portal.di
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.lambdaschool.hackathon_portal.ui.fragments.add.AddHackathonViewModel
 import com.lambdaschool.hackathon_portal.viewmodel.ViewModelProviderFactory
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 import javax.inject.Singleton
 
 @Module
@@ -12,4 +15,10 @@ abstract class ViewModelsModule {
     @Singleton
     @Binds
     abstract fun bindViewModelFactory(viewModelProviderFactory: ViewModelProviderFactory): ViewModelProvider.Factory
+
+    @Singleton
+    @Binds
+    @IntoMap
+    @ViewModelKey(AddHackathonViewModel::class)
+    abstract fun bindsAddHackathonViewModel(viewModel: AddHackathonViewModel): ViewModel
 }
---
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/add/AddHackathonViewModel.kt
@@ -0,0 +1,14 @@
+package com.lambdaschool.hackathon_portal.ui.fragments.add
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.lambdaschool.hackathon_portal.HackathonRepository
+import com.lambdaschool.hackathon_portal.model.Hackathon
+import javax.inject.Inject
+
+class AddHackathonViewModel @Inject constructor(private val repo: HackathonRepository): ViewModel() {
+
+    fun postHackathon(hackathon: Hackathon): LiveData<String> {
+        return repo.postHackathon(hackathon)
+    }
+}
