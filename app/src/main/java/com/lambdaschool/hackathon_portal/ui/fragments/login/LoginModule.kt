ea175ad30e5d6ecdce6b529cbb11210ab9cdc705
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-08 13:32:27 -0500

    refactor Auth0 to use DI

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/login/LoginModule.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/login/LoginModule.kt
@@ -1,7 +1,7 @@
 package com.lambdaschool.hackathon_portal.ui.fragments.login
 
+import com.auth0.android.Auth0
 import com.auth0.android.provider.WebAuthProvider
-import com.lambdaschool.hackathon_portal.App
 import com.lambdaschool.hackathon_portal.di.FragmentScope
 import dagger.Module
 import dagger.Provides
@@ -12,8 +12,8 @@ object LoginModule {
     @FragmentScope
     @Provides
     @JvmStatic
-    fun provideWebAuthProviderLogin() =
-        WebAuthProvider.login(App.auth0)
+    fun provideWebAuthProviderBuilder(auth0: Auth0): WebAuthProvider.Builder =
+        WebAuthProvider.login(auth0)
             .withScheme("demo")
             .withAudience("https://hackathon-portal.herokuapp.com/")
             .withScope("openid profile email offline_access")
