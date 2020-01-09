f2170d9b45671327dc49c043d9c34f232c64dd9c
tjclawson <tjamesclawson@gmail.com>
2020-01-09 17:53:33 -0500

    Refactor models to work with backend schema and endpoints

--- app/src/main/java/com/lambdaschool/hackathon_portal/model/Hackathon.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/model/Hackathon.kt
@@ -1,24 +1,24 @@
 package com.lambdaschool.hackathon_portal.model
 
-data class Hackathon (var name: String,
+data class Hackathon (var id: Int? = null,
+                      var name: String,
                       var description: String,
                       var url: String,
                       var start_date: String,
                       var end_date: String,
                       var location: String,
-                      var is_open: Boolean) {
+                      var is_open: Boolean,
+                      var organizer_id: Int? = null,
+                      var teams: MutableList<Team>? = null,
+                      var admins: MutableList<Admin>? = null,
+                      var individual_devs: MutableList<User>? = null) {
     constructor(
-        id: Int? = null,
         name: String,
         description: String,
         url: String,
         start_date: String,
         end_date: String,
         location: String,
-        is_open: Boolean,
-        organizer_id: Int? = null,
-        teams: MutableList<Team>? = null,
-        admins: MutableList<User>? = null,
-        individual_devs: MutableList<User>? = null
-    ): this (name, description, url, start_date, end_date, location, is_open)
+        is_open: Boolean
+    ): this (null, name, description, url, start_date, end_date, location, is_open)
 }
--- app/src/main/java/com/lambdaschool/hackathon_portal/model/Team.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/model/Team.kt
@@ -3,5 +3,5 @@ package com.lambdaschool.hackathon_portal.model
 data class Team(
     val team_id: Int,
     var team_name: String,
-    var devs: MutableList<User>
+    var devs: MutableList<Dev>
 )
