524de49b103ba140d4bd2259c7c7b2146c061e45
tjclawson <tjamesclawson@gmail.com>
2020-01-10 12:42:47 -0500

    Add details view model

---
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/details/DetailsViewModel.kt
@@ -0,0 +1,9 @@
+package com.lambdaschool.hackathon_portal.ui.fragments.details
+
+import androidx.lifecycle.ViewModel
+import com.lambdaschool.hackathon_portal.repository.HackathonRepository
+import javax.inject.Inject
+
+class DetailsViewModel @Inject constructor(private val repo: HackathonRepository): ViewModel() {
+    
+}
