c60b059e6e692c739f8c45934dd1222548c3a7bf
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-09 21:12:56 -0500

    built out network calls

--- app/src/main/java/com/lambdaschool/hackathon_portal/retrofit/HackathonApiInterface.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/retrofit/HackathonApiInterface.kt
@@ -1,5 +1,6 @@
 package com.lambdaschool.hackathon_portal.retrofit
 
+import com.google.gson.JsonObject
 import com.lambdaschool.hackathon_portal.model.Hackathon
 import com.lambdaschool.hackathon_portal.model.User
 import retrofit2.Call
@@ -15,4 +16,9 @@ interface HackathonApiInterface {
     @GET("users/{id}")
     fun getUser(@Path("id") id: Int,
                 @Header("Authorization") bearerToken: String): Call<User>
+
+    @PUT("users/{id}")
+    fun updateUser(@Path("id") id: Int?,
+                   @Header("Authorization") bearerToken: String,
+                   @Body user: JsonObject): Call<User>
 }
