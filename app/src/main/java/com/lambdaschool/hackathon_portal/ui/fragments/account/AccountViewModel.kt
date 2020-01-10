b99bc302432d33fab2ed6776e2dc1a2c75c55ec5
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-09 21:11:33 -0500

    added Account ViewModel to make network calls

---
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/account/AccountViewModel.kt
@@ -0,0 +1,14 @@
+package com.lambdaschool.hackathon_portal.ui.fragments.account
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.google.gson.JsonObject
+import com.lambdaschool.hackathon_portal.repository.HackathonRepository
+import javax.inject.Inject
+
+class AccountViewModel @Inject constructor(private val repo: HackathonRepository): ViewModel() {
+
+    fun updateUser(jsonObject: JsonObject): LiveData<Boolean> {
+        return repo.updateUser(jsonObject)
+    }
+}
