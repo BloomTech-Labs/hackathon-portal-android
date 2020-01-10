b7ffcb4a57b1e48b6c11da258947a7b7ffcbe212
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-09 21:17:01 -0500

    built out function to construct JsonObject with only properties needing to be updated

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/account/AccountFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/account/AccountFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
+import com.google.gson.JsonObject
 
 import com.lambdaschool.hackathon_portal.R
 import com.lambdaschool.hackathon_portal.model.LoggedInUser
@@ -51,5 +53,44 @@ class AccountFragment : Fragment() {
         edit_text_user_last_name.setText(LoggedInUser.user.last_name)
         edit_text_username.setText(LoggedInUser.user.username)
         edit_text_email_address.setText(LoggedInUser.user.email)
+    }
+
+    private fun buildJsonObject(): JsonObject? {
+        val firstName = edit_text_user_first_name.text.toString()
+        val lastName = edit_text_user_last_name.text.toString()
+        val username = edit_text_username.text.toString()
+        val email = edit_text_email_address.text.toString()
+
+        var counter = 0
+        val jsonObject = JsonObject()
+        if (firstName != LoggedInUser.user.first_name) {
+            jsonObject.addProperty("first_name", firstName)
+            counter++
+        }
+
+        if (lastName != LoggedInUser.user.last_name) {
+            jsonObject.addProperty("last_name", lastName)
+            counter++
+        }
+
+        if (username.isNotEmpty() && username != LoggedInUser.user.username) {
+            jsonObject.addProperty("username", username)
+            counter++
+        }
+
+        if (email.isNotEmpty() && email != LoggedInUser.user.email) {
+            jsonObject.addProperty("email", email)
+            counter++
+        }
+
+        return if (counter > 0) {
+            jsonObject
+        }
+        else {
+            activity?.apply {
+                Toast.makeText(this, "Nothing to update", Toast.LENGTH_SHORT).show()
+            }
+            null
+        }
     }
 }
