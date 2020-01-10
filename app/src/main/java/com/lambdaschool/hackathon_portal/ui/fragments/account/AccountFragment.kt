a51c952d7067e96165a0e68c0fc760d8d8060acf
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-09 21:15:35 -0500

    load viewmodel and setup edit text fields with current info

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/account/AccountFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/account/AccountFragment.kt
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 
 import com.lambdaschool.hackathon_portal.R
+import com.lambdaschool.hackathon_portal.model.LoggedInUser
 import com.lambdaschool.hackathon_portal.ui.MainActivity
+import com.lambdaschool.hackathon_portal.viewmodel.ViewModelProviderFactory
+import kotlinx.android.synthetic.main.fragment_account.*
+import javax.inject.Inject
 
 class AccountFragment : Fragment() {
 
@@ -20,12 +25,31 @@ class AccountFragment : Fragment() {
             .build()
     }
 
+    @Inject
+    lateinit var viewModelProviderFactory: ViewModelProviderFactory
+
+    lateinit var accountViewModel: AccountViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         fragmentComponent.injectAccountFragment(this)
         super.onCreate(savedInstanceState)
+        accountViewModel = ViewModelProviders.of(this, viewModelProviderFactory)
+            .get(AccountViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadUserInfoToEditTextFields()
+    }
+
+    private fun loadUserInfoToEditTextFields() {
+        edit_text_user_first_name.setText(LoggedInUser.user.first_name)
+        edit_text_user_last_name.setText(LoggedInUser.user.last_name)
+        edit_text_username.setText(LoggedInUser.user.username)
+        edit_text_email_address.setText(LoggedInUser.user.email)
     }
 }
