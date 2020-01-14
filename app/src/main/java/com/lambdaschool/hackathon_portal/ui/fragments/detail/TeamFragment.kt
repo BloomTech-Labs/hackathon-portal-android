a0b8342dd6bfec714fe8928007967ee635af860b
tjclawson <tjamesclawson@gmail.com>
2020-01-14 17:36:37 -0500

    Add listener to display team members when holder is clicked in team list adapter

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/detail/TeamFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/detail/TeamFragment.kt
@@ -1,23 +1,32 @@
 package com.lambdaschool.hackathon_portal.ui.fragments.detail
 
 
+import android.animation.LayoutTransition
 import android.content.Context
 import android.os.Bundle
+import android.transition.Fade
+import android.transition.TransitionManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
 
 import com.lambdaschool.hackathon_portal.R
+import com.lambdaschool.hackathon_portal.model.Dev
 import com.lambdaschool.hackathon_portal.model.Team
 import com.lambdaschool.hackathon_portal.ui.MainActivity
 import com.lambdaschool.hackathon_portal.viewmodel.ViewModelProviderFactory
 import kotlinx.android.synthetic.main.fragment_team.*
+import kotlinx.android.synthetic.main.team_list_item_view.*
 import kotlinx.android.synthetic.main.team_list_item_view.view.*
 import javax.inject.Inject
 
@@ -75,6 +84,8 @@ class TeamFragment : Fragment() {
         inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             val nameView: TextView = view.text_view_team_name
             val participantsView: TextView = view.text_view_team_participants
+            val parentView: CardView = view.parent_card_view
+            val linearLayout: LinearLayout = view.linear_layout_list_members
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -88,8 +99,43 @@ class TeamFragment : Fragment() {
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val data = teams[position]
+            var hasBeenExpanded = false
             holder.nameView.text = data.team_name
             holder.participantsView.text = data.devs.size.toString()
+            holder.linearLayout.visibility = View.GONE
+            holder.parentView.setOnClickListener {
+
+                when {
+                    !holder.linearLayout.isVisible && !hasBeenExpanded -> {
+                        data.devs.forEach {
+                            holder.linearLayout.addView(createTeamMemberViews(it))
+                        }
+                        holder.linearLayout.visibility = View.VISIBLE
+                        hasBeenExpanded = true
+                    }
+
+                    !holder.linearLayout.isVisible && hasBeenExpanded -> {
+                        holder.linearLayout.visibility = View.VISIBLE
+                    }
+
+                    holder.linearLayout.isVisible -> {
+                        holder.linearLayout.visibility = View.GONE
+                    }
+                }
+            }
         }
+    }
+
+    private fun createTeamMemberViews(dev: Dev): LinearLayout {
+        val linearLayout = LinearLayout(this.context)
+        val usernameTextView = TextView(this.context)
+        val roleTextView = TextView(this.context)
+        usernameTextView.text = dev.username
+        roleTextView.text = dev.developer_role
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.addView(usernameTextView)
+        linearLayout.addView(roleTextView)
+        linearLayout.setPadding(8, 8, 8, 8)
+        return linearLayout
     }
 }
