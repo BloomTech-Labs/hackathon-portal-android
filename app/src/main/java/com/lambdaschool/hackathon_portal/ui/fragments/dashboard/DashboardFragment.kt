d6d8d61d5193be7c7edb61afe7041ff4957f2d58
msmaitran <itsmaisn@gmail.com>
2020-01-13 17:54:44 -0800

    Update UI for fragment_dashboard

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/dashboard/DashboardFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.graphics.toColorInt
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
@@ -71,6 +72,9 @@ class DashboardFragment : Fragment() {
 
         inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             val nameView: TextView = view.text_view_hackathon_name
+            val locationView: TextView = view.text_view_hackathon_location
+            val startView: TextView = view.text_view_start_date
+            val statusView: TextView = view.text_view_status
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -85,6 +89,13 @@ class DashboardFragment : Fragment() {
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val data = hackathons[position]
             holder.nameView.text = data.name
+            holder.locationView.text = "Location: ${data.location}"
+            holder.startView.text = "Start Date: ${data.start_date}"
+            holder.statusView.text = if (data.is_open) {
+                getString(R.string.status_open)
+            } else {
+                getString(R.string.status_closed)
+            }
             holder.itemView.setOnClickListener {
                 val bundle = Bundle()
                 data.id?.let {
