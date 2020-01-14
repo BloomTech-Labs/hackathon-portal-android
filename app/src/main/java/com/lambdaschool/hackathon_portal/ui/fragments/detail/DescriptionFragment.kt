f8187ed0fa78ceb0b472bf0bc1b0390ccb9497b1
tjclawson <tjamesclawson@gmail.com>
2020-01-13 19:48:44 -0500

    Add logic to display hackathon description

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/detail/DescriptionFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/detail/DescriptionFragment.kt
@@ -6,18 +6,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 
 import com.lambdaschool.hackathon_portal.R
+import com.lambdaschool.hackathon_portal.ui.MainActivity
+import com.lambdaschool.hackathon_portal.viewmodel.ViewModelProviderFactory
+import kotlinx.android.synthetic.main.fragment_description.*
+import javax.inject.Inject
 
-/**
- * A simple [Fragment] subclass.
- */
-class Description : Fragment() {
+class DescriptionFragment : Fragment() {
+
+    private val fragmentComponent by lazy {
+        (activity as MainActivity)
+            .activityComponent
+            .getFragmentComponentBuilder()
+            .bindFragment(this)
+            .build()
+    }
+
+    @Inject
+    lateinit var viewModelProviderFactory: ViewModelProviderFactory
+    lateinit var detailViewModel: DetailViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        fragmentComponent.injectDescriptionFragment(this)
+        super.onCreate(savedInstanceState)
+        detailViewModel = ViewModelProviders.of(this, viewModelProviderFactory).get(DetailViewModel::class.java)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_description, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        detailViewModel.currentHackathon.observe(this, Observer {
+            fragment_description_text_view_description.text = it.description
+        })
+    }
 }
