6b790e4dd619f1939fc872e964d765066774b758
tjclawson <tjamesclawson@gmail.com>
2020-01-14 12:57:35 -0500

    Add OnPageChangeListener to view pager so correct tab will show as selected when user swipes to navigate between tabs

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/detail/DetailFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/detail/DetailFragment.kt
@@ -9,9 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
 import android.widget.Toast
+import androidx.core.view.get
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
 import com.lambdaschool.hackathon_portal.R
@@ -85,6 +87,21 @@ class DetailFragment : Fragment() {
                     view_pager.currentItem = position
                 }
             }
+        })
+
+        view_pager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                fragment_detail_tab_layout.getTabAt(position)?.select()
+            }
+
         })
     }
     private fun updateHackathonViews(hackathon: Hackathon) {
