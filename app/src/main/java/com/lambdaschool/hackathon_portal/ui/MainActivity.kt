44bcadf131611e04cf4dc94d5a9604f576ca48b7
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-15 13:07:41 -0500

    setup app bar configuration to work with nav view and nav controller

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/MainActivity.kt
@@ -5,10 +5,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
 import com.lambdaschool.hackathon_portal.App
 import com.lambdaschool.hackathon_portal.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -34,11 +32,32 @@ class MainActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
     @Inject
     lateinit var navController: NavController
+    @Inject
+    lateinit var navView: NavigationView
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         activityComponent.injectMainActivity(this)
+
+        // Must call so DI modules build & set the AppBar
+        toggle
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_dashboard,
+                R.id.nav_create_hackathon,
+                R.id.nav_user_hackathons,
+                R.id.nav_account/*,
+                R.id.settingsFragment*/,
+                R.id.nav_logout
+            ), drawerLayout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
 //        drawerLayout.addDrawerListener(toggle)
 
@@ -111,7 +130,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(nav_view)) {
+        if (drawerLayout.isDrawerOpen(navView)) {
             drawerLayout.closeDrawers()
 
             //TODO: Need to figure out logic to determine if the current fragment is the
@@ -123,5 +142,9 @@ class MainActivity : AppCompatActivity() {
         */} else {
             super.onBackPressed()
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
