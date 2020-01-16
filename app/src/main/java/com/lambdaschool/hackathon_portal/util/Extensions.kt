8db01e322df4ec110667717f18661cd593e6ef20
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-16 17:26:01 -0500

    added extension function for NavController to customize setPopUpTo in NavOption.Builder before navigating to specified fragment

--- app/src/main/java/com/lambdaschool/hackathon_portal/util/Extensions.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/util/Extensions.kt
@@ -1,9 +1,13 @@
 package com.lambdaschool.hackathon_portal.util
 
 import android.content.Context
+import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.annotation.IdRes
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 
 /**
  * Open the keyboard and focus on a specified view
@@ -25,4 +29,22 @@ fun Context._openSoftKeyboardAndFocus(view: View?) {
  * */
 fun Context._toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+/**
+ * Builds NavOptions to popUpTo and then navigates to specified fragment
+ * @param bundle: Bundle
+ * @param destinationId : @IdRes Int
+ * @param popUpToInclusive : Boolean
+ * @param resId : @IdRes Int
+ * */
+fun NavController._navigateAndPopUpTo(bundle: Bundle,
+                                      @IdRes destinationId: Int,
+                                      popUpToInclusive: Boolean,
+                                      @IdRes resId: Int) {
+    val navOptions = NavOptions.Builder()
+        .setPopUpTo(destinationId, popUpToInclusive)
+        .build()
+
+    this.navigate(resId, bundle, navOptions)
 }
