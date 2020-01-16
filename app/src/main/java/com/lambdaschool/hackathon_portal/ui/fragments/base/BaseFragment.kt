72483d417b553c9c8c90d5c10f32fc8030ba4b35
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-16 10:12:00 -0500

    initialize structure of BaseFragment

---
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/base/BaseFragment.kt
@@ -0,0 +1,45 @@
+package com.lambdaschool.hackathon_portal.ui.fragments.base
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment: Fragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+}
