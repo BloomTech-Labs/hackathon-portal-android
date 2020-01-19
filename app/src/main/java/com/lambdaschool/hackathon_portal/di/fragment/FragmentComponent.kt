2077803dee83f0953506546a8757956bef7dd1d0
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-19 08:03:22 -0500

    Combine Login & Logout modules

--- app/src/main/java/com/lambdaschool/hackathon_portal/di/fragment/FragmentComponent.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/di/fragment/FragmentComponent.kt
@@ -4,14 +4,13 @@ import androidx.fragment.app.Fragment
 import com.lambdaschool.hackathon_portal.ui.fragments.BaseFragment
 import com.lambdaschool.hackathon_portal.ui.fragments.NavDrawerFragment
 import com.lambdaschool.hackathon_portal.ui.fragments.login.LoginFragment
-import com.lambdaschool.hackathon_portal.di.fragment.modules.LoginModule
+import com.lambdaschool.hackathon_portal.di.fragment.modules.WebAuthModule
 import com.lambdaschool.hackathon_portal.ui.fragments.logout.LogoutFragment
-import com.lambdaschool.hackathon_portal.di.fragment.modules.LogoutModule
 import dagger.BindsInstance
 import dagger.Subcomponent
 
 @FragmentScope
-@Subcomponent(modules = [LoginModule::class, LogoutModule::class])
+@Subcomponent(modules = [WebAuthModule::class])
 interface FragmentComponent {
 
     @Subcomponent.Builder
