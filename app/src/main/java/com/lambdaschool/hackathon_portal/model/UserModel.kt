855e69363729dd5d94e6f0f72ac54dc32acba1aa
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-19 16:33:03 -0500

    change file/class name and setup typealiases

--- app/src/main/java/com/lambdaschool/hackathon_portal/model/UserModel.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/model/UserModel.kt
@@ -1,19 +1,22 @@
 package com.lambdaschool.hackathon_portal.model
 
-sealed class User {
+typealias User = UserModel.User
+typealias UserAuth0 = UserModel.Auth0
 
-    data class GetUser(
+sealed class UserModel {
+
+    data class User(
         var id: Int,
         var first_name: String?,
         var last_name: String?,
         var username: String?,
         var email: String,
         var hackathons: MutableList<UserHackathon>
-    ): User()
+    ): UserModel()
 
     class Auth0(
         var id: Int,
         var pictureUrl: String,
         var accessToken: String
-    ): User()
+    ): UserModel()
 }
