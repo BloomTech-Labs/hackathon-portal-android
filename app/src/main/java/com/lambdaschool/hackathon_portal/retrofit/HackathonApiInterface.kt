64e6e8dc0f2a5f14577f9a1deaf05b69f765dc5b
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-19 13:15:39 -0500

    reorder ApiInterface calls

--- app/src/main/java/com/lambdaschool/hackathon_portal/retrofit/HackathonApiInterface.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/retrofit/HackathonApiInterface.kt
@@ -9,11 +9,6 @@ import retrofit2.http.*
 
 interface HackathonApiInterface {
 
-    @POST("hackathons/u/{id}")
-    fun postHackathon(@Path("id") id: Int,
-                      @Header("Authorization") bearerToken: String,
-                      @Body hackathon: Hackathon): Call<Hackathon>
-
     @GET("users/{id}")
     fun getUser(@Path("id") id: Int,
                 @Header("Authorization") bearerToken: String): Call<UserD.UserE>
@@ -27,12 +22,17 @@ interface HackathonApiInterface {
     fun deleteUser(@Path("id") id: Int,
                    @Header("Authorization") bearerToken: String): Call<Deletion>
 
+    @GET("hackathons")
+    fun getAllHackathons(@Header("Authorization") bearerToken: String): Call<MutableList<Hackathon>>
+
     @GET("hackathons/{id}")
     fun getHackathon(@Path("id") id: Int,
                      @Header("Authorization") bearerToken: String): Call<Hackathon>
 
-    @GET("hackathons")
-    fun getAllHackathons(@Header("Authorization") bearerToken: String): Call<MutableList<Hackathon>>
+    @POST("hackathons/u/{id}")
+    fun postHackathon(@Path("id") id: Int,
+                      @Header("Authorization") bearerToken: String,
+                      @Body hackathon: Hackathon): Call<Hackathon>
 
     @PUT("hackathons/{hack_id}/u/{org_id}")
     fun updateHackathon(@Path("hack_id") hack_id: Int,
