21f94683676e5625fc0c465e93c6da0e0806c9ae
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-19 12:49:50 -0500

    update to new user model

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/dashboard/DashboardViewModel.kt
@@ -3,15 +3,15 @@ package com.lambdaschool.hackathon_portal.ui.fragments.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.lambdaschool.hackathon_portal.model.Hackathon
-import com.lambdaschool.hackathon_portal.model.User
+import com.lambdaschool.hackathon_portal.model.UserD
 import com.lambdaschool.hackathon_portal.model.UserHackathon
 import com.lambdaschool.hackathon_portal.repository.HackathonRepository
 import javax.inject.Inject
 
 class DashboardViewModel @Inject constructor(private val repo: HackathonRepository) : ViewModel() {
 
-    fun getUser(id: Int): LiveData<User> {
-        return repo.getUser(id)
+    fun getUser(): LiveData<UserD.UserE> {
+        return repo.getUser()
     }
 
     // Leaving this function because it might be necessary later
