d657f83a36c19094796596197fb0e9c4e76746ee
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-19 12:07:43 -0500

    update ViewModel to use new repository functions

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/login/LoginViewModel.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/login/LoginViewModel.kt
@@ -2,13 +2,24 @@ package com.lambdaschool.hackathon_portal.ui.fragments.login
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.lambdaschool.hackathon_portal.model.User
+import com.lambdaschool.hackathon_portal.model.UserD
 import com.lambdaschool.hackathon_portal.repository.HackathonRepository
 import javax.inject.Inject
 
 class LoginViewModel @Inject constructor(private val repo: HackathonRepository): ViewModel() {
 
-    fun getUser(id: Int): LiveData<User> {
-        return repo.getUser(id)
-    }
+    fun setUserAuth0Id(id: Int) =
+        repo.setUserAuth0Id(id)
+
+    fun setUserAuth0PictureUrl(pictureUrl: String) =
+        repo.setUserAuth0PictureUrl(pictureUrl)
+
+    fun setUserAuth0AccessToken(accessToken: String) =
+        repo.setUserAuth0AccessToken(accessToken)
+
+    fun getUser(): LiveData<UserD.UserE> =
+        repo.getUser()
+
+    fun getUserAuth0PictureUrl() =
+        repo.getUserAuth0PictureUrl()
 }
