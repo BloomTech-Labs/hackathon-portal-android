2b5ad9d907b18aef7f9615e0af89c9dea9fe9f07
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-19 12:16:04 -0500

    rework LoginFragment to use new methods

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/login/LoginFragment.kt
@@ -18,10 +18,9 @@ import com.auth0.android.provider.AuthCallback
 import com.auth0.android.provider.WebAuthProvider
 import com.auth0.android.result.Credentials
 import com.lambdaschool.hackathon_portal.R
-import com.lambdaschool.hackathon_portal.model.CurrentUser
+import com.lambdaschool.hackathon_portal.model.UserD
 import com.lambdaschool.hackathon_portal.ui.fragments.NavDrawerFragment
 import com.lambdaschool.hackathon_portal.util.*
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.GlobalScope
@@ -102,43 +101,59 @@ class LoginFragment : NavDrawerFragment() {
         credentialsManager.getCredentials(object : BaseCallback<Credentials, CredentialsManagerException?> {
 
             override fun onSuccess(credentials: Credentials) {
-                setCurrentUser(credentials)
-                navController._navigateAndPopUpTo(
-                    Bundle(), R.id.nav_login, true, R.id.nav_dashboard
-                )
+                if (setUserAuth0(credentials)) {
+                    getUserData()
+                }
             }
 
             override fun onFailure(error: CredentialsManagerException?) {
                 error?.message?.let {
                     activity?._toastLong(it)
+                    Log.d(TAG, it)
                 }
             }
         })
     }
 
-    private fun setCurrentUser(credentials: Credentials) {
+    private fun setUserAuth0(credentials: Credentials): Boolean {
         val jwt = JWT(credentials.idToken!!)
         val claims: Map<String, Claim> = jwt.claims
 
-        CurrentUser.currentUser.accessToken = credentials.accessToken
+        // Using a counter to ensure that the AccessToken and Id get set,
+        // otherwise nothing past this part will work properly.
+        var counter = 0
 
-        claims.get("sub")?.asString()?.let {
-            val userId = it.split("|")[1]
-            CurrentUser.currentUser.id = userId
-
-            activity?.apply {
-                loginViewModel.getUser(userId.toInt()).observe(this, Observer { response ->
-                    if (response != null) {
-                        navHeaderTitleTextView.text = response.username
-                        navHeaderSubtitleTextView.text = response.email
-                    }
-                })
-            }
+        claims["picture"]?.asString()?.let {
+            loginViewModel.setUserAuth0PictureUrl(it)
         }
 
-        claims.get("picture")?.asString()?.let {
-            CurrentUser.currentUser.pictureURL = it
-            Picasso.get().load(it).into(navHeaderImageView)
+        credentials.accessToken?.let {
+            loginViewModel.setUserAuth0AccessToken(it)
+            counter++
         }
+
+        claims["sub"]?.asString()?.let {
+            loginViewModel.setUserAuth0Id(it.split("|")[1].toInt())
+            counter++
+        }
+        return counter > 1
+    }
+
+    private fun getUserData() {
+        activity?.apply {
+            loginViewModel.getUser().observe(this, Observer { response ->
+                if (response != null) {
+                    setNavDrawerHeader(response)
+                    navController._navigateAndPopUpTo(
+                        Bundle(), R.id.nav_login, true, R.id.nav_dashboard)
+                }
+            })
+        }
+    }
+
+    private fun setNavDrawerHeader(response: UserD.UserE) {
+        setNavDrawerHeaderTitle(response.username)
+        setNavDrawerHeaderSubTitle(response.email)
+        setNavDrawerHeaderImage(loginViewModel.getUserAuth0PictureUrl())
     }
 }
