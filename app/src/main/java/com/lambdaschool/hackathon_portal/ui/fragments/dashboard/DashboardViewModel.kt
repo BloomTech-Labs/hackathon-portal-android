8b95207ab52bdae1f60e8199ae2d8181907d908c
Matthew Nelson <05nelsonm@protonmail.com>
2020-01-20 14:24:06 -0500

    remove unnecessary methods from view model

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/dashboard/DashboardViewModel.kt
@@ -3,23 +3,10 @@ package com.lambdaschool.hackathon_portal.ui.fragments.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.lambdaschool.hackathon_portal.model.Hackathon
-import com.lambdaschool.hackathon_portal.model.User
-import com.lambdaschool.hackathon_portal.model.UserHackathon
 import com.lambdaschool.hackathon_portal.repository.HackathonRepository
-import com.lambdaschool.hackathon_portal.repository.UserRepository
 import javax.inject.Inject
 
-class DashboardViewModel @Inject constructor(private val hackathonRepo: HackathonRepository,
-                                             private val userRepo: UserRepository) : ViewModel() {
-
-    fun getUser(): LiveData<User> {
-        return userRepo.getUserById()
-    }
-
-    // Leaving this function because it might be necessary later
-    fun getUserHackathonList(): LiveData<MutableList<UserHackathon>> {
-        return userRepo.getUserHackathonLiveList()
-    }
+class DashboardViewModel @Inject constructor(private val hackathonRepo: HackathonRepository) : ViewModel() {
 
     fun getAllHackthons(): LiveData<MutableList<Hackathon>> {
         return hackathonRepo.getAllHackathons()
