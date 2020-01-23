f9095297018439d91331dd05ed744149fe525e7c
tjclawson <tjamesclawson@gmail.com>
2020-01-23 12:29:45 -0500

    Add project model, rename and modify Dev to Participant

--- app/src/main/java/com/lambdaschool/hackathon_portal/model/Dev.kt
+++
@@ -1,7 +0,0 @@
-package com.lambdaschool.hackathon_portal.model
-
-data class Dev(
-    var user_id: Int,
-    var username: String,
-    var developer_role: String
-)
---
+++ app/src/main/java/com/lambdaschool/hackathon_portal/model/Project.kt
@@ -0,0 +1,14 @@
+package com.lambdaschool.hackathon_portal.model
+
+data class Project(
+    val project_id: Int,
+    var project_title: String,
+    var project_description: String,
+    var front_end_spots: Int,
+    var back_end_spots: Int,
+    var ios_spots: Int,
+    var android_spots: Int,
+    var data_science_spots: Int,
+    var ux_spots: Int,
+    var participants: MutableList<Participant>
+)
