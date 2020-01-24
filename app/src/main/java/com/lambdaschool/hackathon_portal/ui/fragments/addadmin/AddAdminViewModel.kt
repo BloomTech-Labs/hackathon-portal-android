e76bd5c9301fa6c2ff2de12b6353f3276246b3d8
tjclawson <tjamesclawson@gmail.com>
2020-01-24 17:30:15 -0500

    Add AddAdminViewModel

---
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/addadmin/AddAdminViewModel.kt
@@ -0,0 +1,9 @@
+package com.lambdaschool.hackathon_portal.ui.fragments.addadmin
+
+import androidx.lifecycle.ViewModel
+import com.lambdaschool.hackathon_portal.repository.HackathonRepository
+import javax.inject.Inject
+
+class AddAdminViewModel @Inject constructor(private val repo: HackathonRepository): ViewModel() {
+    
+}
