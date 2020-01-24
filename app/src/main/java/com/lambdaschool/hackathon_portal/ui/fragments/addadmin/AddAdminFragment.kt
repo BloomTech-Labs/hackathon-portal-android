7d933fbea63517e5e39873960d45b75605402468
tjclawson <tjamesclawson@gmail.com>
2020-01-24 17:15:46 -0500

    Add AddAdminFragment and basic layout

---
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/addadmin/AddAdminFragment.kt
@@ -0,0 +1,19 @@
+package com.lambdaschool.hackathon_portal.ui.fragments.addadmin
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.lambdaschool.hackathon_portal.R
+
+class AddAdminFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_add_admin, container, false)
+    }
+
+
+}
