3bb951ad3244d71ade727c70bf09bf6e4581611e
tjclawson <tjamesclawson@gmail.com>
2020-01-24 17:56:18 -0500

    Add basic viewmodel implementation to addAdminFragment

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/addadmin/AddAdminFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/addadmin/AddAdminFragment.kt
@@ -6,10 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 
 import com.lambdaschool.hackathon_portal.R
+import com.lambdaschool.hackathon_portal.ui.fragments.BaseFragment
 
-class AddAdminFragment : Fragment() {
+class AddAdminFragment : BaseFragment() {
+
+    private lateinit var addAdminViewModel: AddAdminViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        addAdminViewModel = ViewModelProviders.of(this, viewModelProviderFactory)
+            .get(AddAdminViewModel::class.java)
+        addAdminViewModel.getAllUsers()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_add_admin, container, false)
--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/addadmin/AddAdminViewModel.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/addadmin/AddAdminViewModel.kt
@@ -1,9 +1,15 @@
 package com.lambdaschool.hackathon_portal.ui.fragments.addadmin
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.lambdaschool.hackathon_portal.model.User
+import com.lambdaschool.hackathon_portal.model.UserModel
 import com.lambdaschool.hackathon_portal.repository.HackathonRepository
 import javax.inject.Inject
 
 class AddAdminViewModel @Inject constructor(private val repo: HackathonRepository): ViewModel() {
-    
+
+    fun getAllUsers(): LiveData<MutableList<User>> {
+        return repo.getAllUser()
+    }
 }
