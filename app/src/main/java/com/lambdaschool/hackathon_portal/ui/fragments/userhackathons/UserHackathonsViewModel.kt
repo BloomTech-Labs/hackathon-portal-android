7f65647244fe950ccef8bdda5132b0e3f9730cfb
tjclawson <tjamesclawson@gmail.com>
2020-01-29 12:32:02 -0500

    Add progressbar and refresh functionality to UserHackathonsFragment

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/userhackathons/UserHackathonsViewModel.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/userhackathons/UserHackathonsViewModel.kt
@@ -2,6 +2,7 @@ package com.lambdaschool.hackathon_portal.ui.fragments.userhackathons
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.lambdaschool.hackathon_portal.model.User
 import com.lambdaschool.hackathon_portal.model.UserHackathon
 import com.lambdaschool.hackathon_portal.repository.HackathonRepository
 import javax.inject.Inject
@@ -10,5 +11,9 @@ class UserHackathonsViewModel @Inject constructor(private val repo: HackathonRep
 
     fun getUserHackathonList(): LiveData<MutableList<UserHackathon>> {
         return repo.getUserHackathonList()
+    }
+
+    fun getUser(): LiveData<User> {
+        return repo.getUser()
     }
 }
