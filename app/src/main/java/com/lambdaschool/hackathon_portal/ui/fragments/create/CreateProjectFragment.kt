f38c1a4cda19a92f10deb60082b9f7f8056632eb
tjclawson <tjamesclawson@gmail.com>
2020-01-30 12:02:18 -0500

    Display alert dialog letting user know that project is only being submitted and will need to be approved by organizer

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/create/CreateProjectFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/create/CreateProjectFragment.kt
@@ -11,6 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.lambdaschool.hackathon_portal.R
 import com.lambdaschool.hackathon_portal.model.Project
 import com.lambdaschool.hackathon_portal.ui.fragments.BaseFragment
+import com.lambdaschool.hackathon_portal.util.buildAlertDialog
 import com.lambdaschool.hackathon_portal.util.openSoftKeyboardAndFocus
 import com.lambdaschool.hackathon_portal.util.toastShort
 import kotlinx.android.synthetic.main.fragment_create_project.*
@@ -36,43 +37,56 @@ class CreateProjectFragment : BaseFragment() {
 
         val hackathonId = arguments?.getInt("hackathon_id")
 
+        val title = "Note"
+        val message = "This will only submit your project to the organizers. You and other users " +
+                "will not be able to sign up for the project until it has been approved by an " +
+                "organizer. If you are the organizer for this hackathon, the project will be " +
+                "automatically approved."
+
+
         button_fragment_create_project_create_project.setOnClickListener {
-            if (hackathonId != null) {
-                val projectTitle = edit_text_project_title.text.toString()
-                val projectDescription = edit_text_project_description.text.toString()
-                val frontEndSpots = getIntegerFromEditText(edit_text_front_end_spots)
-                val backEndSpots = getIntegerFromEditText(edit_text_back_end_spots)
-                val iosSpots = getIntegerFromEditText(edit_text_ios_spots)
-                val androidSpots = getIntegerFromEditText(edit_text_android_spots)
-                val dataScienceSpots = getIntegerFromEditText(edit_text_data_science_spots)
-                val uxDesignerSpots = getIntegerFromEditText(edit_text_ux_designer_spots)
-
-                val project =
-                    Project(
-                        projectTitle,
-                        projectDescription,
-                        false,
-                        createProjectViewModel.getUserObject().id,
-                        hackathonId,
-                        frontEndSpots,
-                        backEndSpots,
-                        iosSpots,
-                        androidSpots,
-                        dataScienceSpots,
-                        uxDesignerSpots)
-
-                createProjectViewModel.postProject(project).observe(viewLifecycleOwner, Observer {
-                    if (it != null) {
-                        if (it) {
-                            activity?.toastShort("Project created!")
-                            navController.popBackStack()
-                        } else {
-                            activity?.toastShort("Failed to create Project")
-                        }
-                    }
-                })
-            }
+            activity?.buildAlertDialog(title, message, {
+                hackathonId?.let { postProject(hackathonId) }
+            }, {}, "Ok", "Cancel")
         }
+    }
+
+    private fun postProject(id: Int) {
+        val projectTitle = edit_text_project_title.text.toString()
+        val projectDescription = edit_text_project_description.text.toString()
+        val frontEndSpots = getIntegerFromEditText(edit_text_front_end_spots)
+        val backEndSpots = getIntegerFromEditText(edit_text_back_end_spots)
+        val iosSpots = getIntegerFromEditText(edit_text_ios_spots)
+        val androidSpots = getIntegerFromEditText(edit_text_android_spots)
+        val dataScienceSpots = getIntegerFromEditText(edit_text_data_science_spots)
+        val uxDesignerSpots = getIntegerFromEditText(edit_text_ux_designer_spots)
+
+        val project =
+            Project(
+                projectTitle,
+                projectDescription,
+                false,
+                createProjectViewModel.getUserObject().id,
+                id,
+                frontEndSpots,
+                backEndSpots,
+                iosSpots,
+                androidSpots,
+                dataScienceSpots,
+                uxDesignerSpots
+            )
+
+        createProjectViewModel.postProject(project)
+            .observe(viewLifecycleOwner, Observer {
+                if (it != null) {
+                    if (it) {
+                        activity?.toastShort("Project submitted!")
+                        navController.popBackStack()
+                    } else {
+                        activity?.toastShort("Failed to submit Project")
+                    }
+                }
+            })
     }
 
     private fun getIntegerFromEditText(editText: EditText): Int {
