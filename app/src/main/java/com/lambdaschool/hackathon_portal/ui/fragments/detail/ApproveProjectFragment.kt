e759603fb9cf2bcf5965b5d54732bf14f3cdcf10
tjclawson <tjamesclawson@gmail.com>
2020-01-30 12:56:05 -0500

    Add basic layout for ApproveProjectFragment

--- app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/detail/ApproveProjectFragment.kt
+++ app/src/main/java/com/lambdaschool/hackathon_portal/ui/fragments/detail/ApproveProjectFragment.kt
@@ -9,16 +9,9 @@ import android.view.ViewGroup
 
 import com.lambdaschool.hackathon_portal.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class ApproveProjectFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_approve_project, container, false)
     }
 
